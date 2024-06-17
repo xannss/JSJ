e633f429ed04588f1fff00b3f837cda499e987da
xannss <xannshiro@gmail.com>
2024-06-17 16:03:16 +0700

    menambahkan route utama

---
+++ resources/views/utama.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <h1>JSJ</h1>
+        <nav>
+        <ul>
+            <li>Utama</li>
+            <li>About</li>
+            <li>Profil</li>
+        </ul>
+        </nav>
+    </header>
+</body>
+</html>
