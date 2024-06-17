2446c827bca86f67af64adf332d4cd507b759635
xannss <xannshiro@gmail.com>
2024-06-17 16:58:41 +0700

    style css

--- resources/views/utama.blade.php
+++ resources/views/utama.blade.php
@@ -3,13 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="css/style.css">
     <title>Document</title>
 </head>
 <body>
     <header>
-        <h1>JSJ</h1>
-        <nav>
+        <h1 class="logo">JSJ</h1>
+        <nav class="nav_about">
         <ul>
             <li>Utama</li>
             <li>About</li>
@@ -18,7 +18,7 @@
         </nav>
     </header>
     <section>
-        <p>hello word</p>
+        
     </section>
 </body>
 </html>
