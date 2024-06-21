6be2093910f13ea2d99096a8bd272e40269a9700
xannss <bihet21264@elahan.com>
2024-06-21 07:34:58 +0700

    tak tambahi section ku

--- resources/views/utama.blade.php
+++ resources/views/utama.blade.php
@@ -11,14 +11,22 @@
         <nav class="navbar">
         <a href="#" class="logo">JSJ.</a></h1>
         <ul class="menu">
-            <li><a href="#">Utama</a></li>
+            <li><a href="#">Home</a></li>
             <li><a href="#">About</a></li>
-            <li><a href="#">Tentang</a></li>
+            <li><a href="#">contact us</a></li>
         </ul>
         </nav>
     </header>
-    <section class="lana">
-      <p>hello word</p>
+    <section class="home">
+      <div class="content">
+        <h1>
+            Jasa Super Joki
+        </h1>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi facere reprehenderit, atque officiis incidunt fugit aut quas impedit iure nostrum earum distinctio molestiae tenetur similique ab quaerat provident repudiandae. Sed!
+        </p>
+        <button>order now</button>
+      </div>
     </section>
     <section>
         ojan
