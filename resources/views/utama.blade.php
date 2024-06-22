81bf78fe6bcfe30c11a514458202e20bfc17bd35
Fahmi503 <fahmi.firmansyah503@gmail.com>
2024-06-23 00:02:58 +0700

    HTML BERES TINGGAL PERAPIAN SAMA CSS

--- resources/views/utama.blade.php
+++ resources/views/utama.blade.php
@@ -33,7 +33,47 @@
         ojan
     </section>
     <section>
-        fahmi pengen nenen
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Nama Anggota</title>
+</head>
+<body>
+<div class="card-container">
+    <div class="card">
+        <img src="" alt="">
+        <div class="card-content">
+            <h3>Muhammad Fariz Rizky</h3>
+            <p>Sebagai Tampilan akhir</p>
+            <a href="" class="btn">Read more</a>
+        </div>
+    </div>
+    <div class="card">
+        <img src="" alt="">
+        <div class="card-content">
+            <h3>Ahmmad Maulana Syarifuddin</h3>
+            <p>Sebagai Tampilan Awal</p>
+            <a href="" class="btn">Read more</a>
+        </div>
+    </div>
+    <div class="card">
+        <img src="" alt="">
+        <div class="card-content">
+            <h3>Muhammad Fahmi Firmansyah</h3>
+            <p>Sebagai Membuat Tampilan Profil Profil</p>
+            <a href="" class="btn">Read more</a>
+        </div>
+    </div>
+</div>    
+
+
+</body>
+</html>
+
+
     </section>
 </body>
 </html>
